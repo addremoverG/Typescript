fa737626bacf0c17346193e3a672817b40356eb3
maciekdabrowski <maciejdabrowski99@gmail.com>
2025-12-04 11:40:47 +0100

    removed unnecessary files

--- Attendants/Maciej/z1/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-(async () => {
-    console.log('Hello World');
-    console.log(process.env.test);
-})().catch((err) => {
-    console.log(err instanceof Error ? err.message : err);
-    process.exit(1);
-});
